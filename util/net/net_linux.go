4516eaf4cf50d5fbba4619572cdd3bb582fccfd7
Krzysztof Nazarewski <gpg@kdn.im>
2024-11-18 09:17:59 +0100

    add NB_SKIP_SOCKET_MARK
    https://github.com/netbirdio/netbird/issues/2530

--- util/net/net_linux.go
+++ util/net/net_linux.go
@@ -4,8 +4,13 @@ package net
 
 import (
 	"fmt"
+	"os"
 	"syscall"
+
+	log "github.com/sirupsen/logrus"
 )
+
+const envSkipSocketMark = "NB_SKIP_SOCKET_MARK"
 
 // SetSocketMark sets the SO_MARK option on the given socket connection
 func SetSocketMark(conn syscall.Conn) error {
@@ -28,16 +33,28 @@ func SetRawSocketMark(conn syscall.RawConn) error {
 	}
 
 	if setErr != nil {
-		return fmt.Errorf("set SO_MARK: %w", setErr)
+		return fmt.Errorf("set SO_MARK failed: %w", setErr)
 	}
 
 	return nil
 }
 
 func SetSocketOpt(fd int) error {
+	log.Debugf("Attempting to set SO_MARK on fd %d", fd)
 	if CustomRoutingDisabled() {
+		log.Infof("Custom routing is disabled, skipping SO_MARK")
 		return nil
 	}
 
-	return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_MARK, NetbirdFwmark)
+	// Check for the new environment variable
+	if skipSocketMark := os.Getenv(envSkipSocketMark); skipSocketMark == "true" {
+		log.Info("NB_SKIP_SOCKET_MARK is set to true, skipping SO_MARK")
+		return nil
+	}
+
+	err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_MARK, NetbirdFwmark)
+	if err == nil {
+		log.Debugf("Successfully set SO_MARK to %d", NetbirdFwmark)
+	}
+	return err
 }
